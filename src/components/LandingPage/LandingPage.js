2bbf54e8875ca2cc7e06f6494b94738de380c3b4
lilymaechling <lily.s.maechling.23@dartmouth.edu>
2022-10-16 23:51:27 -0400

    styled landing

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -5,8 +5,39 @@ import './landing.css'
 
 const LandingPage = () => {
 
+  const [theme, setTheme] = useState('light');
+
   return (
-    <div> hi landing....
+    <div className="landing-page" data-theme={theme}>
+      <div className="title-content" data-theme={theme}>
+        <h1>Welcome to ConvoCode</h1>
+        <h2>Redefining Python Developer tools.</h2>
+        <div className="file-buttons" data-theme={theme}>
+          <button>
+            Upload Python File
+          </button>
+          <button>
+            New Python File
+          </button>
+        </div>
+      </div>
+      <div className="floating-text">
+        <div>
+          <p id="floating-cf" data-theme={theme}>"create function"</p>
+        </div>
+        <div>
+          <p id="floating-bc" data-theme={theme}>"build class"</p>
+        </div>
+        <div>
+          <p id="floating-dc" data-theme={theme}>"debug code"</p>
+        </div>
+        <div>
+          <p id="floating-ar" data-theme={theme}>"analyze runtime"</p>
+        </div>
+        <div>
+          <p id="floating-sa" data-theme={theme}>"stackoverflow analysis"</p>
+        </div>
+      </div>
     </div>
   );
 };
