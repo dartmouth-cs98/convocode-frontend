9e0fcf31eb7672fc69cd041c8b4c7d9491216b64
Melissa Valencia <melissa.valencia.23@dartmouth.edu>
2022-11-02 19:13:59 -0400

    code clean up

--- src/state/actions/fileManagement 2.js
+++
@@ -1,9 +0,0 @@
-export const ActionTypes = {
-    FILE_NAME: 'FILE_NAME',
-  };
-  
-export function createFileName(name) {
-    return (dispatch) => {
-      dispatch({ type: ActionTypes.FILE_NAME, payload: name});
-    };
-  }
