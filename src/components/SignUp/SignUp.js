a9a98812af03e399184abe2e0b9649c6caa857b9
lilymaechling <lily.s.maechling.23@dartmouth.edu>
2023-01-22 11:29:44 -0500

    starting formik?

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,30 +1,87 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { connect } from 'react-redux';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
 import HeaderBar from "../HeaderBar/HeaderBar";
 
 import './signup.css'
 
-
 const SignUp = (props) => {
-  const [name, setName] = useState();
-  const [username, setUsername] = useState();
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
-  const [email2, setEmail2] = useState();
-  const [password2, setPassword2] = useState();
+  // const [name, setName] = useState();
+  // const [username, setUsername] = useState();
+  // const [email, setEmail] = useState();
+  // const [password, setPassword] = useState();
+  // const [email2, setEmail2] = useState();
+  // const [password2, setPassword2] = useState();
+  // const [emailMatchError, setEmailMatchError] = useState(false);
+  // const [emailRegError, setEmailRegError] = useState(false);
+  // const [passMatchError, setPassMatchError] = useState(false);
 
-  const handleSubmit = () => {
-    console.log("user info sent")
-  }
+  // const handleSubmit = () => {
+  //   if (email === email2 && password === password2 && validEmail.test(email)) {
+  //     alert('form submitted');
+  //     resetErrors()
+  //     resetAllFields();
+  //   } else {
+  //     console.log("improper data")
+  //     if (email !== email2) {
+  //       setEmailMatchError(true)
+  //       setEmail("")
+  //       setEmail2("")
+  //     }
+  //     if (password !== password2) {
+  //       setPassMatchError(true)
+  //       setPassword("")
+  //       setPassword2("")
+  //     }
+  //     if (validEmail.test(email)) {
+  //       setEmailRegError(true)
+  //       setEmail("")
+  //       setEmail2("")
+  //     }
+  //   }
+  // }
+
+  // const resetAllFields = () => {
+  //   setName("")
+  //   setUsername("")
+  //   setEmail("")
+  //   setEmail2("")
+  //   setPassword("")
+  //   setPassword2("")
+  // }
+
+  // const resetErrors = () => {
+  //   setEmailMatchError(false)
+  //   setEmailRegError(false)
+  //   setPassMatchError(false)
+  // }
+
+
+  const SignupSchema = Yup.object().shape({
+    name: Yup.string()
+      .min(2, 'Too Short')
+      .max(50, 'Too Long')
+      .required('Required'),
+    username: Yup.string()
+      .min(2, 'Too Short')
+      .max(50, 'Too Long')
+      .required('Required'),
+    email: Yup.string().email('Invalid email').required('Required'),
+    password: Yup.string()
+      .required('No password provided.')
+      .min(8, 'Password is too short - should be 8 chars minimum.')
+      .matches(/[a-zA-Z]/, 'Password can only contain Latin letters.'),
+  });
 
   return (
     <div className="sign-up" data-theme={props.lightMode ? 'light' : 'dark'}>
       <HeaderBar />
       <div className="content">
         <h1>Convo<span id="sage">C</span><span id="sky">o</span><span id="grape">d</span><span id="pumpkin-spice">e</span></h1>
-        <form onSubmit={() => handleSubmit()}>
+        {/* <form onSubmit={() => handleSubmit()}>
           <label>
             <h3>Name:</h3>
             <input type="text" value={name} id="one" onChange={(e) => setName(e.target.data)} />
@@ -53,7 +110,35 @@ const SignUp = (props) => {
             <input type="submit" />
             <NavLink to="/signin" id="link">Sign In</NavLink>
           </div>
-        </form>
+        </form> */}
+        <Formik
+          initialValues={{
+            name: '',
+            username: '',
+            email: '',
+            password: '',
+          }}
+          validationSchema={SignupSchema}
+          onSubmit={values => {
+            console.log(values);
+          }}
+        >
+          {({ errors, touched }) => (
+            <Form>
+              <Field name="firstName" />
+              {errors.firstName && touched.firstName ? (
+                <div>{errors.firstName}</div>
+              ) : null}
+              <Field name="lastName" />
+              {errors.lastName && touched.lastName ? (
+                <div>{errors.lastName}</div>
+              ) : null}
+              <Field name="email" type="email" />
+              {errors.email && touched.email ? <div>{errors.email}</div> : null}
+              <button type="submit">Submit</button>
+            </Form>
+          )}
+        </Formik>
       </div>
     </div>
   );
