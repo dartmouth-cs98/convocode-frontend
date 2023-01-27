70292df3304bc0cd7fe9e6f56171cbb5ebdba0b4
Annabel Revers <annabel.m.revers.23@dartmouth.edu>
2023-01-26 21:52:05 -0500

    new combined reducer and action files for project information

---
+++ src/state/reducers/project.js
@@ -0,0 +1,31 @@
+import { ActionTypes } from '../actions';
+
+const initialState = {
+  javascript: "",
+  html: "",
+  css: "",
+  projectId: ""
+};
+
+const ProjectReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionTypes.ADD_JAVASCRIPT_CODE:
+      return { ...state, javascript: action.payload };
+    case ActionTypes.INSERT_JAVASCRIPT_CODE:
+      return { ...state, javascript: state.javascript + action.payload };
+    case ActionTypes.ADD_HTML_CODE:
+      return { ...state, html: action.payload };
+    case ActionTypes.INSERT_HTML_CODE:
+      return { ...state, html: state.html + action.payload };
+    case ActionTypes.ADD_CSS_CODE:
+      return { ...state, css: action.payload };
+    case ActionTypes.INSERT_CSS_CODE:
+      return { ...state, css: state.css + action.payload };
+    case ActionTypes.ADD_PROJECT_ID:
+      return { ...state, projectId: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default ProjectReducer;
