3362739f52391f5df76b7ab780110a9bc2884251
Annabel Revers <annabel.m.revers.23@dartmouth.edu>
2023-01-29 19:32:19 -0500

    deleted unecessary files

--- src/state/actions/convodex.js
+++
@@ -1,9 +0,0 @@
-export const ActionTypes = {
-  ADD_ENTRY: 'ADD_ENTRY',
-};
-
-export function addConvodexEntry(entry) {
-  return (dispatch) => {
-    dispatch({ type: ActionTypes.ADD_ENTRY, payload: entry });
-  };
-}
