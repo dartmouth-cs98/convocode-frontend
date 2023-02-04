af17382d1ac6422fa787657c3155131b88bbd0f6
lilymaechling <lily.s.maechling.23@dartmouth.edu>
2023-02-04 15:08:35 -0500

    conditional header bar rendering

--- src/components/HeaderBar/HeaderBar.js
+++ src/components/HeaderBar/HeaderBar.js
@@ -7,17 +7,34 @@ import "./header.css"
 const HeaderBar = (props) => {
   return (
     <div className='header-bar' data-theme={props.lightMode ? 'light' : 'dark'}>
-      <NavLink to="/"><h1>Convo<span id="sage">C</span><span id="sky">o</span><span id="grape">d</span><span id="pumpkin-spice">e</span></h1></NavLink>
-      <div className='header-buttons'>
-        <NavLink to="/community"><button id="documentation">Community</button></NavLink>
-        <NavLink to="/editor" state={{ name: "newfile.py" }}><button id="IDE">Open IDE</button></NavLink>
-        <NavLink to="/signin"><button id="signin">Sign In</button></NavLink>
-      </div>
-    </div>
+      {
+        props.user.username ?
+          <>
+            <NavLink to="/"><h1>Convo<span id="sage">C</span><span id="sky">o</span><span id="grape">d</span><span id="pumpkin-spice">e</span></h1></NavLink>
+            <div className='header-buttons'>
+              <NavLink to="/community"><button id="community">Community</button></NavLink>
+              <NavLink to="/editor" state={{ name: "newfile.py" }}><button id="IDE">Open IDE</button></NavLink>
+              <NavLink to="/profile"><button id="profile">@{props.user.username}</button></NavLink>
+            </div>
+          </>
+          :
+          <>
+            <NavLink to="/"><h1>Convo<span id="sage">C</span><span id="sky">o</span><span id="grape">d</span><span id="pumpkin-spice">e</span></h1></NavLink>
+            <div className='header-buttons'>
+              <NavLink to="/community"><button id="documentation">Community</button></NavLink>
+              <NavLink to="/editor" state={{ name: "newfile.py" }}><button id="IDE">Open IDE</button></NavLink>
+              <NavLink to="/signin"><button id="signin">Sign In</button></NavLink>
+            </div>
+          </>
+      }
+    </div >
   );
 };
 const mapStateToProps = (reduxstate) => {
-  return { lightMode: reduxstate.settings.lightMode };
+  return {
+    lightMode: reduxstate.settings.lightMode,
+    user: reduxstate.user,
+  };
 };
 
 export default connect(mapStateToProps, {})(HeaderBar);
