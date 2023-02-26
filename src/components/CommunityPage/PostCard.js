42269b11123cda0d88199ee18f1a5309afbe8ea7
Annabel Revers <annabel.m.revers.23@dartmouth.edu>
2023-02-26 17:18:09 -0500

    randomized colors on community page

--- src/components/CommunityPage/PostCard.js
+++ src/components/CommunityPage/PostCard.js
@@ -5,13 +5,16 @@ import './community.css'
 
 const Post = (props) => {
 
+  // randomize color of post card
+  const colors = [ "postUnicorn", "postEasyA", "postGrape", "postSky", "postSage", "postBus", "postPumpkin" ];
+  const colorInt = Math.floor(Math.random() * 7);
+  const postClass = colors[colorInt];
+
   const navigate = useNavigate();
   let tag = props.item.tags.length > 0 ? props.item.tags[0].toString().toLowerCase() : "undefined"
 
-  console.log(props.item)
-
   return (
-    <div key={props.key} id={tag} className="post" onClick={() => navigate(`/project/${props.item.id}`)}>
+    <div key={props.key} id={tag} className={postClass} onClick={() => navigate(`/project/${props.item.id}`)}>
       <div className="titles">
         <h3>{props.item.title}</h3>
         <span className="username">@{props.item.username}</span>
