5727c17d3d42e28d6d7596d95be415a798a9b663
lilymaechling <lily.s.maechling.23@dartmouth.edu>
2023-03-03 11:49:24 -0500

    fixed individual post

--- src/components/EditorWindow/WebOutput.js
+++ src/components/EditorWindow/WebOutput.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import './index.css'
 import { connect } from 'react-redux';
 import { addJavascriptCode, addHTMLCode, addCSSCode, addCleanedJavascript } from "../../state/actions";
@@ -7,14 +7,17 @@ import Run from '../../resources/play.png'
 
 
 const WebOutput = (props) => {
-    const [iframeSrc, setIframeSrc] = useState(null);
-  
-    const getGeneratedPageURL = ({ html, css, js }) => {
+  const [iframeSrc, setIframeSrc] = useState(null);
+
+  console.log(iframeSrc)
+
+  const getGeneratedPageURL = ({ html, css, js }) => {
     const getBlobURL = (code, type) => {
-        const blob = new Blob([code], { type });
-        return URL.createObjectURL(blob);
+      const blob = new Blob([code], { type });
+      return URL.createObjectURL(blob);
     };
-    
+    console.log(js)
+    console.log(html)
     const cssURL = getBlobURL(css, 'text/css')
     const jsURL = getBlobURL(js, 'text/javascript')
     const source = `
@@ -34,38 +37,39 @@ const WebOutput = (props) => {
 
   useEffect(() => {
     const url = getGeneratedPageURL({
-        html: props.htmlCode,
-        css: props.cssCode,
-        js: props.cleanedCode,
-      });
+      html: props.htmlCode,
+      css: props.cssCode,
+      js: props.javaCode,
+    });
     setIframeSrc(url);
 
-  }, [props.htmlCode, props.cssCode, props.javascriptCode, props.cleanedCode]);
+  }, [props.htmlCode, props.cssCode, props.javaCode, props.cleanedCode]);
 
 
 
   return (
     <div className="output-window" >
       <Iframe url={iframeSrc}
-          // width="640px"
-          // height="320px"
-          width={props.width}
-          height={props.height}
-          id=""
-          className="iframe"
-          display="block"
-          position="relative"/>
+        // width="640px"
+        // height="320px"
+        width={props.width}
+        height={props.height}
+        id=""
+        className="iframe"
+        display="block"
+        position="relative"
+        onClick={() => alert("in iframe")} />
     </div>
   );
 };
 
 const mapStateToProps = (reduxstate) => {
-    return {
-      javaCode: reduxstate.project.javaCode,
-      htmlCode: reduxstate.project.htmlCode,
-      cssCode: reduxstate.project.cssCode,
-      cleanedCode: reduxstate.project.cleanedCode,
-    };
+  return {
+    javaCode: reduxstate.project.javaCode,
+    htmlCode: reduxstate.project.htmlCode,
+    cssCode: reduxstate.project.cssCode,
+    cleanedCode: reduxstate.project.cleanedCode,
   };
-  
+};
+
 export default connect(mapStateToProps, { addJavascriptCode, addHTMLCode, addCSSCode, addCleanedJavascript })(WebOutput);
